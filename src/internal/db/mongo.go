b93ae44c6cbb443e63c82492256087e7b506c335
“Gutto <guttolaudie@gmail.com>
2024-10-24 14:44:25 -0300

    feat: connection logic MongoDB

---
+++ src/internal/db/mongo.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"context"
+	"log"
+	"time"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectMongo(uri string) (*mongo.Database, error) {
+	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+        err = client.Ping(ctx, nil)
+        if err != nil {
+                return nil, err
+        }
+
+        log.Println("Connected to MongoDB")
+        return client.Database("go-embedded-system"), nil
+}
